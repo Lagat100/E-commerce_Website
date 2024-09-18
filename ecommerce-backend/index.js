574b6eac1d9a2401d053b36b22799af86e9e79d1
Lagat100 <weldonkiprop1@gmail.com>
2024-09-18 10:49:54 +0300

    Updating to fix product routes

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -11,10 +11,12 @@ const app = express();
 app.use(express.json());
 
 const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
-const productRoutes = require('./routes/productRoutes');
+
 const orderRoutes = require('./routes/orderRoutes');
 
 app.use('/api/products', productRoutes);
