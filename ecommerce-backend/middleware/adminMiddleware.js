eedfb1ae1065f66ec6510325588136668acb930b
Lagat100 <weldonkiprop1@gmail.com>
2024-09-19 12:56:57 +0300

    Update administration rolees

--- ecommerce-backend/middleware/adminMiddleware.js
+++ ecommerce-backend/middleware/adminMiddleware.js
@@ -1,10 +1,22 @@
-const admin = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-      next();  // User is admin, proceed to the next middleware or route handler
-    } else {
-      res.status(403).json({ message: 'Access denied: Admins only' });
+const User = require('../models/User');
+
+const admin = async (req, res, next) => {
+  try {
+    const { userId } = req.body;  
+
+    // Find the user in the database by their ID
+    const user = await User.findOne({ _id: userId });
+    console.log(user)
+
+    if (!user || user.role !== 'admin') {
+      return res.status(403).json({ message: 'Access denied: Admins only dd' });
     }
-  };
+
+    next();
+  } catch (error) {
   
-  module.exports = admin;
-  
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = admin;
